438a7392789640423bdc679c6faaa51f4b2803fd
Gopesh <medayilgopesh@gmail.com>
2025-11-22 13:45:15 +0100

    refactor: remove unused App import from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import { Login } from './components/Login/Login.tsx'
 import { Dashboard } from './components/DashBoard/DashBoard.tsx'
